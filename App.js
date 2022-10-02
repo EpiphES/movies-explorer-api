8b9a9fc83911685191f7f159b48523cbb2bc4c3a
Ekaterina Epiphanova <epiph@yandex.ru>
2022-10-02 13:28:35 +0500

    feat: start server and database

---
+++ App.js
@@ -0,0 +1,44 @@
+// require("dotenv").config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+// const cookieParser = require("cookie-parser");
+// const { errors } = require("celebrate");
+// const cors = require("cors");
+// const router = require('./routes');
+// const errorHandler = require('./middlewares/error');
+// const { requestLogger, errorLogger } = require("./middlewares/logger");
+const { PORT, MONGO_URI } = require('./utils/constants');
+
+const app = express();
+
+app.use(express.json());
+// app.use(cookieParser());
+
+// app.use(
+//   cors({
+//     origin: [
+//       "http://epiphany.nomoredomains.sbs",
+//       "https://epiphany.nomoredomains.sbs",
+//     ],
+//     credentials: true,
+//   })
+// );
+// app.use(requestLogger);
+// app.use('/', router);
+// app.use(errorLogger);
+// app.use(errors());
+// app.use(errorHandler);
+
+async function main() {
+  await mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: false,
+  });
+  await app.listen(PORT);
+
+  // eslint-disable-next-line no-console
+  console.log(`App listening on port ${PORT}`);
+}
+
+main();
