2f952147d4066c87b3b657cb3c6a68d5c826e183
Ekaterina Epiphanova <epiph@yandex.ru>
2022-10-05 22:16:08 +0500

    fix: disable rateLimiter

--- controllers/users.js
+++ controllers/users.js
@@ -88,7 +88,6 @@ const login = (req, res, next) => {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
         sameSite: 'none',
-        // secure: true,
       });
       res.send({ data: user.toJSON() });
     })
