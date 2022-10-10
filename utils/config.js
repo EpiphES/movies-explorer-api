d15d0a6c87fcaa40a2a6ee84867e7d04dcbcc47f
Ekaterina Epiphanova <epiph@yandex.ru>
2022-10-10 17:57:33 +0500

    fix: database name

--- utils/config.js
+++ utils/config.js
@@ -1,4 +1,4 @@
-const MONGO_URI_DEV = 'mongodb://localhost:27017/bitfilmsdb';
+const MONGO_URI_DEV = 'mongodb://localhost:27017/moviesdb';
 const JWT_SECRET_DEV = 'never share your secret';
 
 module.exports = {
